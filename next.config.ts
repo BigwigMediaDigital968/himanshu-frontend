1c6085dbdfef41740b9124770fff4eb95c8fc8c6
lalit nath <lalitwebd@bigwigmediadigital.com>
2026-05-07 17:24:44 +0530

    varicose page redirection

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,20 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["images.unsplash.com", "plus.unsplash.com", "res.cloudinary.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/services/varicose-vein-treatment-in-delhi",
+        destination: "/services/varicose-veins-treatment-in-gurgaon",
+        permanent: true, // 301 redirect (SEO friendly)
+      },
+      {
+        source: "/services/varicose-vein-treatment-in-varanasi",
+        destination: "/services/varicose-veins-treatment-in-gurgaon",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
