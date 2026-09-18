c7d84c6c67a1043e0cfa1e8ef015c5dfc940f6b5
lalit nath <lalitwebd@bigwigmediadigital.com>
2026-09-18 17:48:09 +0530

    video update

--- app/workshop/avf-care/page.tsx
+++ app/workshop/avf-care/page.tsx
@@ -138,7 +138,7 @@ export default function AVFCareWorkshop() {
           playsInline
           className="absolute inset-0 w-full h-full object-cover"
         >
-          <source src="/avf-care-video.mp4" type="video/mp4" />
+          <source src="/workshop-18.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
